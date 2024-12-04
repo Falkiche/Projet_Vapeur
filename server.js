7a4b65d412722cb682e64623e10b35f168f9b0ef
Falkiche <charly.lebras19@gmail.com>
2024-12-04 16:50:31 +0100

    Premier essai de base de donnée

--- server.js
+++ server.js
@@ -1,23 +1,37 @@
-const express = require("express");
-const { PrismaClient } = require("@prisma/client");
-const bodyParser = require("body-parser");
-const hbs = require("hbs");
-const path = require("path");
+    const express = require("express");
+    const { PrismaClient } = require("@prisma/client");
+    const bodyParser = require("body-parser");
+    const path = require("path");
 
-const app = express();
-const PORT = 3000;
+    const app = express();
+    const prisma = new PrismaClient();
+    const PORT = 3000;
 
-const prisma = new PrismaClient();
-// On définit un middleware pour parser les données des requêtes entrantes.
-// Cela permet de récupérer les données envoyées via des formulaires et les rendre disponibles dans req.body.
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+    // On définit un middleware pour parser les données des requêtes entrantes.
+    // Cela permet de récupérer les données envoyées via des formulaires et les rendre disponibles dans req.body.
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname,"public", "addGame.html"));;
+    });
+    app.get("/addGame", async (req, res) => {
+        const game = await prisma.games.findMany();
+        res.json(game);
+    });
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname,"addGame.html"));;
-});
+    app.post("/addGame", async (req, res) => {
+        console.log("SALUT")
+        const { name } = req.body;
+        try {
+            await prisma.games.create({
+                data: { name },
+            }); // Ici on ne stock pas le retour de la requête, mais on attend quand même son exécution
+            res.status(201).redirect("/addGame"); // On redirige vers la page des tâches
+        } catch (error) {
+            console.error(error);
+            res.status(400).json({ error: "Game creation failed" });
+        }
+    });
 
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
